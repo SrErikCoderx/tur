a9e15567adbf02dc69e03c7b8de9dcf4a8f365b5
Hernandez <171220766+SrErikCoderx@users.noreply.github.com>
2026-06-21 21:48:43 -0500

    openjdk-8: extract NDK_TOOLCHAIN variable, add READELF and NDK clang path

--- tur/openjdk-8/build.sh
+++ tur/openjdk-8/build.sh
@@ -45,7 +45,8 @@ termux_step_setup_toolchain() {
 			libxv-dev libxxf86vm-dev libxtst-dev libxt-dev libice-dev libsm-dev
 		touch "$hostpkgs_marker"
 	fi
-	export PATH="$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+	export NDK_TOOLCHAIN="$NDK/toolchains/llvm/prebuilt/linux-x86_64"
+	export PATH="$NDK_TOOLCHAIN/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 	export STRIP=llvm-strip
 	export READELF=llvm-readelf
 	export JAVA_HOME="$TERMUX_PKG_HOSTBUILD_DIR"
